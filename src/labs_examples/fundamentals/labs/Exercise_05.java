dd5b9692a5e32441b937ea74700febf0647d1c23
fullstackjunkie <dirtroaddeveloper@gmail.com>
2024-12-16 13:28:21 -0500

    Finished exercise 03 and 04, and 05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,16 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int len = str.length();
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean eq = str.equals(str2);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String concat = str.concat(str2);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String rep = str2.replace('l', 'r');
+
 
     }
 
